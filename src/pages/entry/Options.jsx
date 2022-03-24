0d78d4dd66c776adf336938eff479e061b8e0947
ZebGirouard <zeb@codecademy.com>
2022-03-24 07:54:59 -0600

    ScoopOption test passing

---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ScoopOption } from "./ScoopOption";
+import { Row } from "react-bootstrap";
+export const Options = ({ optionType }) => {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((res) => setItems(res.data))
+      .catch((err) => console.log(err));
+  }, [optionType]);
+  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+
+  return (
+    <Row>
+      {items.map((item) => (
+        <ItemComponent
+          key={item.name}
+          name={item.name}
+          imagePath={item.imagePath}
+        />
+      ))}
+    </Row>
+  );
+};
